f9c128694ac4dceb54c91f2c6549a923f1606b87
Jeff Boschee <jeff@mindcloud.com>
2017-03-25 14:00:23 -0700

    added saving bugs to firebase

--- app/bugs/bug-detail/bug-detail.component.ts
+++ app/bugs/bug-detail/bug-detail.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
+import { BugService } from '../service/bug.service';
+import { Bug } from '../model/bug';
+
+import { forbiddenStringValidator } from '../../shared/validation/forbidden-string.validator';
 
 @Component({
     moduleId: module.id,
@@ -10,21 +15,54 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class BugDetailComponent implements OnInit{ 
     private modalId = "bugModal";
     private bugForm: FormGroup;
+    @Input() currentBug = new Bug(null, null, 1, 1, null, null, null, null, null);
+
+    constructor(private formB: FormBuilder, private bugService: BugService) {
+
+    }
 
     ngOnInit() {
         this.configureForm();
     }
 
-    configureForm() {
-        this.bugForm = new FormGroup({
-            title: new FormControl(null, Validators.required),
-            status: new FormControl(1, Validators.required),
-            severity: new FormControl(1, Validators.required),
-            description: new FormControl(null, Validators.required)
-        });
+    configureForm(bug?: Bug) {
+        // this.bugForm = new FormGroup({
+        //     title: new FormControl(null, [Validators.required, forbiddenStringValidator(/puppy/i)]),
+        //     status: new FormControl(1, Validators.required),
+        //     severity: new FormControl(1, Validators.required),
+        //     description: new FormControl(null, Validators.required)
+        // });
+        if (bug) {
+            this.currentBug = bug;
+        }
+        this.bugForm = this.formB.group({
+            title: [this.currentBug.title, [Validators.required, forbiddenStringValidator(/puppy/i)]],
+            status: [this.currentBug.status, Validators.required],
+            severity: [this.currentBug.severity, Validators.required],
+            description: [this.currentBug.description, Validators.required]
+        })
     }
 
     submitForm() {
         console.log(this.bugForm);
+        this.addBug();
+    }
+
+    addBug() {
+        this.currentBug.title = this.bugForm.value["title"];
+        this.currentBug.status = this.bugForm.value["status"];
+        this.currentBug.severity = this.bugForm.value["severity"];
+        this.currentBug.description = this.bugForm.value["description"];
+        this.bugService.addBug(this.currentBug);
+        this.freshForm();
+    }
+
+    freshForm() {
+        this.bugForm.reset({ status: 1, severity: 1});
+        this.cleanBug();
+    }
+
+    cleanBug() {
+        this.currentBug = new Bug(null, null, 1, 1, null, null, null, null, null);
     }
 }
--- app/bugs/service/bug.service.ts
+++ app/bugs/service/bug.service.ts
@@ -24,4 +24,17 @@ export class BugService {
             });
         });
     }
+
+    addBug(bug: Bug) {
+        const newBugRef = this.bugsDbRef.push();
+        newBugRef.set({
+            title: bug.title,
+            status: bug.status,
+            severity: bug.severity,
+            description: bug.description,
+            createdBy: 'Jeff',
+            createdDate: Date.now()
+        })
+        .catch(err => console.error("Unable to add bug to Firebase - ", err));
+    }
 }
